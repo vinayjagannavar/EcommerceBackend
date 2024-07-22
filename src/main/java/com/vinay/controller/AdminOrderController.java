622013fa254892dc6e0cb65b3688834fd8ac9fac
vinayjagannavar <vjagannavar@gmail.com>
2024-07-21 22:25:57 -0400

    Admin Order Product

--- src/main/java/com/vinay/controller/AdminOrderController.java
+++ src/main/java/com/vinay/controller/AdminOrderController.java
@@ -32,37 +32,37 @@ public class AdminOrderController {
 	}
 	
 	@PutMapping("/{orderId}/confirmed")
-	public ResponseEntity<Order> confirmedOrderHandler(@PathVariable Long orderid,@RequestHeader("Authorization") String jwt) throws OrderException{
+	public ResponseEntity<Order> confirmedOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws OrderException{
 		
-		Order order = orderService.confirmedOrder(orderid);
+		Order order = orderService.confirmedOrder(orderId);
 		return new ResponseEntity<>(order,HttpStatus.OK);
 	}
 	
 	@PutMapping("/{orderId}/ship")
-	public ResponseEntity<Order> shippedOrderHandler(@PathVariable Long orderid,@RequestHeader("Authorization") String jwt) throws OrderException{
+	public ResponseEntity<Order> shippedOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws OrderException{
 		
-		Order order = orderService.shippedOrder(orderid);
+		Order order = orderService.shippedOrder(orderId);
 		return new ResponseEntity<>(order,HttpStatus.OK);
 	}
 	
 	@PutMapping("/{orderId}/deliver")
-	public ResponseEntity<Order> deliverOrderHandler(@PathVariable Long orderid,@RequestHeader("Authorization") String jwt) throws OrderException{
+	public ResponseEntity<Order> deliverOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws OrderException{
 		
-		Order order = orderService.deliveredOrder(orderid);
+		Order order = orderService.deliveredOrder(orderId);
 		return new ResponseEntity<>(order,HttpStatus.OK);
 	}
 	
 	@PutMapping("/{orderId}/cancel")
-	public ResponseEntity<Order> cancelOrderHandler(@PathVariable Long orderid,@RequestHeader("Authorization") String jwt) throws OrderException{
+	public ResponseEntity<Order> cancelOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws OrderException{
 		
-		Order order = orderService.cancelledOrder(orderid);
+		Order order = orderService.cancelledOrder(orderId);
 		return new ResponseEntity<>(order,HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{orderId}/delete")
-	public ResponseEntity<ApiResponse> deleteOrderHandler(@PathVariable Long orderid,@RequestHeader("Authorization") String jwt) throws OrderException{
+	public ResponseEntity<ApiResponse> deleteOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws OrderException{
 		
-		orderService.deleteOrder(orderid);
+		orderService.deleteOrder(orderId);
 		
 		ApiResponse res = new ApiResponse();
 		res.setMessage("Order deleted successfully");
